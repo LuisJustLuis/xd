4e882d41c8ce0b1556b33517e1619e626edea450
LuisJustLuis <91627841+LuisJustLuis@users.noreply.github.com>
2022-02-01 13:30:16 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,17 @@
+function doSomething (list: any[]) {
+	
+}
+let boy_list = ["ethanol", "sanwich", "xd"]
+let girl_list = ["that time of month", "coil", "flobber"]
+let neutral_list = ["aluminum ", "floride", "gold"]
+game.splash("Welcome to Baby Name generator 2022")
+let Gender = game.askForString("What is the gender of the baby?")
+if (Gender == "boy") {
+	
+} else if (Gender == "girl") {
+	
+} else if (Gender == "neutral") {
+	
+} else {
+	
+}
