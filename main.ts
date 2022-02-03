05f11b793e434fe0bca41d1d6973cc5ef03fca62
LuisJustLuis <91627841+LuisJustLuis@users.noreply.github.com>
2022-02-03 12:19:44 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
-function doSomething (list: any[]) {
+function pickName (list: any[]) {
 	
 }
 let boy_list = ["ethanol", "sanwich", "xd"]
 let girl_list = ["that time of month", "coil", "flobber"]
 let neutral_list = ["aluminum ", "floride", "gold"]
 game.splash("Welcome to Baby Name generator 2022")
-let Gender = game.askForNumber("What is the gender of the baby? Press 1 for boy. 2 for girl, and 3 for neutral ")
+game.splash("What is the gender of the baby?")
+let Gender = game.askForNumber("Press 1 for boy. 2 for girl, and 3 for neutral ", 1)
 if (Gender == 1) {
 	
 } else if (Gender == 2) {
